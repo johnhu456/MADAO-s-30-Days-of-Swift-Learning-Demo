328f2be8f58f9f3d4384576e8f4c1d73f27c068a
fenghou <johnhu456@163.com>
2016-05-10 15:00:41 +0800

    Finish DAY29

---
+++ 30 Days Of Swift/DAY29/iMessageImagePicker/iMessageImagePicker/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  iMessageImagePicker
+//
+//  Created by MADAO on 16/4/15.
+//  Copyright © 2016年 MADAO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imgHeadIcon: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func handleIconButtonOnClicked(sender: AnyObject) {
+        
+        
+    }
+
+}
+
